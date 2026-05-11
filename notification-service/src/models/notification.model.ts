ad1de34e02058c882bae666e4e8702d6d45e0b85
Suhaila Thasni <suhailathasni0@gmail.com>
2026-05-11 16:28:43 +0530

    Update notification.model.ts

--- notification-service/src/models/notification.model.ts
+++ notification-service/src/models/notification.model.ts
@@ -1,3 +1,175 @@
+// import { DataTypes, Model, Optional } from "sequelize";
+// import sequelize from "../config/db";
+
+// /* =======================
+//    INTERFACE
+// ======================= */
+
+// interface INotification {
+
+//   id: number;
+
+//   userId?: number;
+//   hospitalId?: number;
+//   labId?: number;
+//   staffId?: number;
+//   pharmacyId?: number;
+//   doctorId?: number;
+
+//   message: string;
+
+//   userIsRead: boolean;
+//   hospitalIsRead: boolean;
+//   labIsRead: boolean;
+//   staffIsRead: boolean;
+//   pharmacyIsRead: boolean;
+//   doctorIsRead: boolean;
+// }
+
+// /* =======================
+//    OPTIONAL FIELDS
+// ======================= */
+
+// type NotificationCreationAttributes =
+//   Optional<
+//     INotification,
+//     | "id"
+//     | "userIsRead"
+//     | "hospitalIsRead"
+//     | "doctorIsRead"
+//     | "labIsRead"
+//     | "pharmacyIsRead"
+//     | "staffIsRead"
+//   >;
+
+// /* =======================
+//    MODEL CLASS
+// ======================= */
+
+// class Notification
+//   extends Model<
+//     INotification,
+//     NotificationCreationAttributes
+//   >
+//   implements INotification
+// {
+
+//   public id!: number;
+
+//   public userId?: number;
+//   public hospitalId?: number;
+//   public labId?: number;
+//   public staffId?: number;
+//   public pharmacyId?: number;
+//   public doctorId?: number;
+
+//   public message!: string;
+
+//   public userIsRead!: boolean;
+//   public hospitalIsRead!: boolean;
+//   public labIsRead!: boolean;
+//   public staffIsRead!: boolean;
+//   public pharmacyIsRead!: boolean;
+//   public doctorIsRead!: boolean;
+// }
+
+// /* =======================
+//    INIT
+// ======================= */
+
+// Notification.init(
+
+//   {
+
+//     id: {
+//       type: DataTypes.INTEGER,
+//       autoIncrement: true,
+//       primaryKey: true,
+//     },
+
+//     userId: {
+//       type: DataTypes.INTEGER,
+//       allowNull: true,
+//     },
+
+//     hospitalId: {
+//       type: DataTypes.INTEGER,
+//       allowNull: true,
+//     },
+
+//     labId: {
+//       type: DataTypes.INTEGER,
+//       allowNull: true,
+//     },
+
+//     staffId: {
+//       type: DataTypes.INTEGER,
+//       allowNull: true,
+//     },
+
+//     pharmacyId: {
+//       type: DataTypes.INTEGER,
+//       allowNull: true,
+//     },
+
+//     doctorId: {
+//       type: DataTypes.INTEGER,
+//       allowNull: true,
+//     },
+
+//     message: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+
+//     /* READ STATUS */
+
+//     userIsRead: {
+//       type: DataTypes.BOOLEAN,
+//       defaultValue: false,
+//     },
+
+//     hospitalIsRead: {
+//       type: DataTypes.BOOLEAN,
+//       defaultValue: false,
+//     },
+
+//     labIsRead: {
+//       type: DataTypes.BOOLEAN,
+//       defaultValue: false,
+//     },
+
+//     staffIsRead: {
+//       type: DataTypes.BOOLEAN,
+//       defaultValue: false,
+//     },
+
+//     pharmacyIsRead: {
+//       type: DataTypes.BOOLEAN,
+//       defaultValue: false,
+//     },
+
+//     doctorIsRead: {
+//       type: DataTypes.BOOLEAN,
+//       defaultValue: false,
+//     },
+
+//   },
+
+//   {
+//     sequelize,
+//     modelName: "Notification",
+//     tableName: "notification",
+//     timestamps: true,
+//   }
+
+// );
+
+// export default Notification;
+
+
+
+
 import { DataTypes, Model, Optional } from "sequelize";
 import sequelize from "../config/db";
 
@@ -9,41 +181,34 @@ interface INotification {
 
   id: number;
 
-  userId?: number;
-  hospitalId?: number;
-  labId?: number;
-  staffId?: number;
-  pharmacyId?: number;
-  doctorId?: number;
+  userIds?: number[];
+
+  hospitalIds?: number[];
+
+  labIds?: number[];
+
+  staffIds?: number[];
+
+  pharmacyIds?: number[];
+
+  doctorIds?: number[];
+
+  adminIds?: number[];
+
+  superAdminIds?: number[];
 
   message: string;
-
-  userIsRead: boolean;
-  hospitalIsRead: boolean;
-  labIsRead: boolean;
-  staffIsRead: boolean;
-  pharmacyIsRead: boolean;
-  doctorIsRead: boolean;
 }
 
 /* =======================
-   OPTIONAL FIELDS
+   OPTIONAL
 ======================= */
 
 type NotificationCreationAttributes =
-  Optional<
-    INotification,
-    | "id"
-    | "userIsRead"
-    | "hospitalIsRead"
-    | "doctorIsRead"
-    | "labIsRead"
-    | "pharmacyIsRead"
-    | "staffIsRead"
-  >;
+  Optional<INotification, "id">;
 
 /* =======================
-   MODEL CLASS
+   MODEL
 ======================= */
 
 class Notification
@@ -56,21 +221,23 @@ class Notification
 
   public id!: number;
 
-  public userId?: number;
-  public hospitalId?: number;
-  public labId?: number;
-  public staffId?: number;
-  public pharmacyId?: number;
-  public doctorId?: number;
+  public userIds?: number[];
+
+  public hospitalIds?: number[];
+
+  public labIds?: number[];
+
+  public staffIds?: number[];
+
+  public pharmacyIds?: number[];
+
+  public doctorIds?: number[];
+
+  public adminIds?: number[];
+
+  public superAdminIds?: number[];
 
   public message!: string;
-
-  public userIsRead!: boolean;
-  public hospitalIsRead!: boolean;
-  public labIsRead!: boolean;
-  public staffIsRead!: boolean;
-  public pharmacyIsRead!: boolean;
-  public doctorIsRead!: boolean;
 }
 
 /* =======================
@@ -87,71 +254,57 @@ Notification.init(
       primaryKey: true,
     },
 
-    userId: {
-      type: DataTypes.INTEGER,
+    userIds: {
+      type: DataTypes.JSONB,
       allowNull: true,
+      defaultValue: [],
     },
 
-    hospitalId: {
-      type: DataTypes.INTEGER,
+    hospitalIds: {
+      type: DataTypes.JSONB,
       allowNull: true,
+      defaultValue: [],
     },
 
-    labId: {
-      type: DataTypes.INTEGER,
+    labIds: {
+      type: DataTypes.JSONB,
       allowNull: true,
+      defaultValue: [],
     },
 
-    staffId: {
-      type: DataTypes.INTEGER,
+    staffIds: {
+      type: DataTypes.JSONB,
       allowNull: true,
+      defaultValue: [],
     },
 
-    pharmacyId: {
-      type: DataTypes.INTEGER,
+    pharmacyIds: {
+      type: DataTypes.JSONB,
       allowNull: true,
+      defaultValue: [],
     },
 
-    doctorId: {
-      type: DataTypes.INTEGER,
+    doctorIds: {
+      type: DataTypes.JSONB,
       allowNull: true,
+      defaultValue: [],
+    },
+
+    adminIds: {
+      type: DataTypes.JSONB,
+      allowNull: true,
+      defaultValue: [],
+    },
+
+    superAdminIds: {
+      type: DataTypes.JSONB,
+      allowNull: true,
+      defaultValue: [],
     },
 
     message: {
       type: DataTypes.STRING,
       allowNull: false,
-    },
-
-    /* READ STATUS */
-
-    userIsRead: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-
-    hospitalIsRead: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-
-    labIsRead: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-
-    staffIsRead: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-
-    pharmacyIsRead: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-
-    doctorIsRead: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
     },
 
   },
