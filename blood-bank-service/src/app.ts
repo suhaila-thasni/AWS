6f34729f5d721385f89dea1871b7dd40f7685b0d
Suhaila Thasni <suhailathasni0@gmail.com>
2026-05-11 10:11:11 +0530

    Update app.ts

--- blood-bank-service/src/app.ts
+++ blood-bank-service/src/app.ts
@@ -16,7 +16,7 @@ app.use(morgan("dev"));
 
 // CORS
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173","https://hostahospital.com"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
