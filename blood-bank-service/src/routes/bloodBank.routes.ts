6b44dfc436a1bcc7364fa58308dbe05dfb3ee3a8
Muhammed Safvan MP <134296048+MuhammedSafvanMP@users.noreply.github.com>
2026-05-15 13:50:20 +0530

    Update bloodBank.routes.ts

--- blood-bank-service/src/routes/bloodBank.routes.ts
+++ blood-bank-service/src/routes/bloodBank.routes.ts
@@ -14,7 +14,7 @@ const router = Router();
 
 // CRUD — all routes require authentication
 router.post("/blood-banks",  createOrUpdateStock);
-router.get("/blood-banks",    "create"), getAllStock);
+router.get("/blood-banks",    getAllStock);
 router.get("/blood-banks/hospital/:hospitalId",  getStocksByHospitalId);
 router.get("/blood-banks/:id", getStockById);
 router.put("/blood-banks/:id", updateStockById);
