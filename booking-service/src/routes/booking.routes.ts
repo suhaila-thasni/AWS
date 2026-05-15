ccfff1d4b4603a8c959b3880ecccfe37b3e93fd5
Muhammed Safvan MP <134296048+MuhammedSafvanMP@users.noreply.github.com>
2026-05-15 15:25:26 +0530

    Update booking.routes.ts

--- booking-service/src/routes/booking.routes.ts
+++ booking-service/src/routes/booking.routes.ts
@@ -4,7 +4,7 @@ import {
   getanBooking,
   updateData,
   bookingDelete,
-  getBooking,
+  getBookings,
  
 } from "../controllers/booking.controllers";
 import { authenticate } from "../middleware/authenticate";
@@ -15,15 +15,11 @@ import { checkPermission } from "../middleware/role.middleware";
 const router = Router();
 
 
-
-
-
 // CRUD - Accessible by authenticated Users and Staff
-router.post("/booking", authenticate, Registeration);
-router.get("/booking", authenticate, getBooking);
+router.post("/booking", Registeration);
+router.get("/booking", getBookings);
 router.get("/booking/:id", authenticate, checkPermission("booking", "view" ), getanBooking);
-router.put("/booking/:id", authenticate, checkPermission("booking", "edit" ), updateData);
+router.put("/booking/:id",  updateData);
 router.delete("/booking/:id", authenticate, checkPermission("booking", "delete" ), bookingDelete);
 
 export default router;
-
