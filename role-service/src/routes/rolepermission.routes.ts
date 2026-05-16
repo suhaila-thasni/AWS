21d0b7b0b2c6550cd490a741891a757bf2eedb5b
Muhammed Safvan MP <134296048+MuhammedSafvanMP@users.noreply.github.com>
2026-05-16 13:58:38 +0530

    Update rolepermission.routes.ts

--- role-service/src/routes/rolepermission.routes.ts
+++ role-service/src/routes/rolepermission.routes.ts
@@ -17,9 +17,9 @@ const router = Router();
 // CRUD
 
 router.post("/rolepermission",  createRolepermission);
-router.get("/rolepermission",  getRolepermission);
+router.get("/rolepermission", getRolepermission);
 router.get("/rolepermission/:id",  getanRolepermission);
-router.put("/rolepermission/:id",  updateData);
+router.put("/rolepermission/:id", updateData);
 router.delete("/rolepermission/:id",  rolepermissionDelete);
 
 export default router;
