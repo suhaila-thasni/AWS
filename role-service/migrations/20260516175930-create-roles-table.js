c445b64cdaec7b226300247c5e510a2cf85b02f4
Muhammed Safvan MP <134296048+MuhammedSafvanMP@users.noreply.github.com>
2026-05-16 23:32:48 +0530

    Create 20260516175930-create-roles-table.js

---
+++ role-service/migrations/20260516175930-create-roles-table.js
@@ -0,0 +1,61 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("roles", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      description: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+
+      hospitalId: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+
+      labId: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+
+      pharmacyId: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+
+      isActive: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("roles");
+  },
+};
