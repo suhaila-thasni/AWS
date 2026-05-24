2e7ad399055125c83e08355cd2fbcd75f273962f
Muhammed Safvan MP <134296048+MuhammedSafvanMP@users.noreply.github.com>
2026-05-24 13:04:54 +0530

    Update 20260524065054-add-imageUrl-to-speciality.js

--- speciality-service/migrations/20260524065054-add-imageUrl-to-speciality.js
+++ speciality-service/migrations/20260524065054-add-imageUrl-to-speciality.js
@@ -6,33 +6,21 @@ module.exports = {
       type: Sequelize.STRING,
       allowNull: true,
     });
-  },
 
-
-    async up(queryInterface, Sequelize) {
     await queryInterface.addColumn('specialitys', 'isDelete', {
       type: Sequelize.BOOLEAN,
-         defaultValue: false,
+      defaultValue: false,
     });
-  },
 
-      async up(queryInterface, Sequelize) {
     await queryInterface.addColumn('specialitys', 'isActive', {
       type: Sequelize.BOOLEAN,
-         defaultValue: true,
+      defaultValue: true,
     });
   },
-
-
 
   async down(queryInterface, Sequelize) {
     await queryInterface.removeColumn('specialitys', 'imageUrl');
-  },
-    async down(queryInterface, Sequelize) {
     await queryInterface.removeColumn('specialitys', 'isDelete');
-  },
-     async down(queryInterface, Sequelize) {
     await queryInterface.removeColumn('specialitys', 'isActive');
   }
-
 };
