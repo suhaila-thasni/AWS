c83e23bbb9cf8af48f2df63d9f0fb330cde71397
Muhammed Safvan MP <134296048+MuhammedSafvanMP@users.noreply.github.com>
2026-05-25 13:51:09 +0530

    Update 20260423130000-rename-patientId-to-userId-in-booking.js

--- booking-service/migrations/20260423130000-rename-patientId-to-userId-in-booking.js
+++ booking-service/migrations/20260423130000-rename-patientId-to-userId-in-booking.js
@@ -3,13 +3,13 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    const tableInfo = await queryInterface.describeTable("booking");
+    const tableInfo = await queryInterface.describeTable("bookings");
 
     if (tableInfo.patientId && !tableInfo.userId) {
-      await queryInterface.renameColumn("booking", "patientId", "userId");
+      await queryInterface.renameColumn("bookings", "patientId", "userId");
     } else if (!tableInfo.userId) {
       // If patientId doesn't exist either, create userId from scratch
-      await queryInterface.addColumn("booking", "userId", {
+      await queryInterface.addColumn("bookings", "userId", {
         type: Sequelize.INTEGER,
         allowNull: false,
       });
@@ -17,6 +17,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.renameColumn("booking", "userId", "patientId");
+    await queryInterface.renameColumn("bookings", "userId", "patientId");
   },
 };
