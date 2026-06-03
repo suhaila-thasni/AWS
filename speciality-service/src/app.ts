9eb1e841bf6f6cf45cdbbaffd45f0ee6d2b038b9
Muhammed Safvan MP <134296048+MuhammedSafvanMP@users.noreply.github.com>
2026-06-03 16:56:56 +0530

    Update app.ts

--- speciality-service/src/app.ts
+++ speciality-service/src/app.ts
@@ -1,84 +1,3 @@
-// import express, { Request, Response, NextFunction } from "express";
-// import cors from "cors";
-
-// import helmet from "helmet";
-// import specialityRoutes from "./routes/speciality.routes";
-// import { requestLogger } from "./middleware/logger.middleware";
-// import { logger } from "./utils/logger";
-// import { env } from "./config/env";
-
-// const app = express();
-
-// // Security middleware
-// app.use(helmet());
-
-
-// // Request Tracking & Logging
-// app.use(requestLogger);
-
-
-
-
-// // can be changed if logic changes
-
-// // CORS
-// app.use(cors({
-//   origin: ["http://localhost:5173"], // Allowing local dev and prospective production
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   credentials: true,
-// }));
-
-
-// app.use(express.json({ limit: "10mb" }));
-// app.use(express.urlencoded({ limit: "10mb", extended: true }));
-
-// // ROUTES
-// app.use("/", specialityRoutes);
-
-
-// // Health check endpoint
-// app.get("/health", (req: Request, res: Response) => {
-//   res.status(200).json({
-//     status: "healthy",
-//     service: "speciality-service",
-//     timestamp: new Date().toISOString(),
-//     uptime: process.uptime(),
-//     environment: env.NODE_ENV
-//   });
-// });
-
-
-// // 404 handler
-// app.use((req: Request, res: Response, next: NextFunction) => {
-//   res.status(404).json({
-//     status: 404,
-//     message: "Requested speciality-related resource not found",
-
-//   });
-// });
-
-
-// // Global Error handler with Winston
-// app.use((err: any, req: any, res: Response, next: NextFunction) => {
-//   logger.error("Server error", {
-//     requestId: req.id,
-//     message: err.message,
-//     stack: err.stack,
-//   });
-
-//   res.status(err.status || 500).json({
-//     success: false,
-//     message: "Internal Server Error in Speciality Service",
-//     error: env.NODE_ENV === "development" ? err : {}, // Still show object in dev, hide details in prod
-
-//   });
-// });
-
-// export default app;
-
-
-
-
 import express, {
     Request,
     Response,
@@ -90,6 +9,8 @@ import helmet from "helmet";
 // import cookieParser from "cookie-parser";
 
 import specialityRoutes from "./routes/speciality.routes";
+import categoryRoutes from "./routes/category.routes";
+
 
 import { requestLogger } from "./middleware/logger.middleware";
 
@@ -155,6 +76,8 @@ app.use(
  * ROUTES
  */
 app.use("/", specialityRoutes);
+app.use("/", categoryRoutes);
+
 
 /**
  * HEALTH
