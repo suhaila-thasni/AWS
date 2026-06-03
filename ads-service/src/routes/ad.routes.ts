06efeafbaf7212dcf3ae10e905166860d19beec8
Muhammed Safvan MP <134296048+MuhammedSafvanMP@users.noreply.github.com>
2026-06-03 19:09:55 +0530

    Update ad.routes.ts

--- ads-service/src/routes/ad.routes.ts
+++ ads-service/src/routes/ad.routes.ts
@@ -11,10 +11,10 @@ import { checkPermission } from "../middleware/role.middleware";
 
 const router = Router();
 
-router.post("/ads",  createAd);
+router.post("/ads", checkPermission("ad", "create"), createAd);
 router.get("/ads",  getAds);
-router.get("/ads/:id", authenticate, checkPermission("ad", "view"), getSingleAd);
+router.get("/ads/:id",  checkPermission("ad", "view"), getSingleAd);
 router.put("/ads/:id",  updateAd);
-router.delete("/ads/:id", authenticate, checkPermission("ad", "delete"), deleteAd);
+router.delete("/ads/:id", checkPermission("ad", "delete"), deleteAd);
 
 export default router;
