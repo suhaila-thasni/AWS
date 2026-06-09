b4c0ecbb894c5b1f4cc933fe04d38339a2966705
Muhammed Safvan MP <134296048+MuhammedSafvanMP@users.noreply.github.com>
2026-06-09 16:39:54 +0530

    Update medicin-remainder.queue.ts

--- bulmq-service/src/queues/medicin-remainder.queue.ts
+++ bulmq-service/src/queues/medicin-remainder.queue.ts
@@ -1,5 +1,5 @@
 import { Queue } from "bullmq";
-import connection from "../config/redis";
+import { connection } from "../config/redis";
 
 export const medicinQueue = new Queue("medicin-queue", {
   connection,
