975416df8fe5753753302bded44963af838b386e
Muhammed Safvan MP <134296048+MuhammedSafvanMP@users.noreply.github.com>
2026-06-09 16:18:24 +0530

    Update redis.ts

--- bulmq-service/src/config/redis.ts
+++ bulmq-service/src/config/redis.ts
@@ -1,14 +1,46 @@
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+
+// const connection = {
+//   host: process.env.REDIS_HOST,
+//   port: Number(process.env.REDIS_PORT),
+//   username: process.env.REDIS_USERNAME,
+//   password: process.env.REDIS_PASSWORD,
+//   tls: {}
+// };
+
+// export default connection;
+
 import dotenv from "dotenv";
+import Redis from "ioredis";
 
 dotenv.config();
 
+export const connection = new Redis(process.env.REDIS_URL!, {
+  maxRetriesPerRequest: null,
+});
 
-const connection = {
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT),
-  username: process.env.REDIS_USERNAME,
-  password: process.env.REDIS_PASSWORD,
-  tls: {}
-};
 
-export default connection;
+
+connection.on("connect", () => {
+  console.log("Redis connected ✔️");
+});
+
+connection.on("error", (err) => {
+  console.error("Redis error ❌", err);
+});
+
+async function testRedis() {
+  try {
+    await connection.set("test_key", "hello");
+    const value = await connection.get("test_key");
+
+    console.log("Redis working ✔️ Value =", value);
+  } catch (err) {
+    console.error("Redis NOT working ❌", err);
+  }
+}
+
+testRedis();
