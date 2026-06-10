331b649998a4f6ad338d94a72af05137327c76ea
Muhammed Safvan MP <134296048+MuhammedSafvanMP@users.noreply.github.com>
2026-06-10 13:58:26 +0530

    Update prescription.controller.ts

--- user-service/src/controllers/prescription.controller.ts
+++ user-service/src/controllers/prescription.controller.ts
@@ -32,8 +32,6 @@ export const createPrescription: any = asyncHandler(async (req: Request, res: Re
       const {
       temperature, pulse, respiratoryRate, spo2, height, weight, waist
     } = req.body;
-
- console.log(req.body, "hiii");
  
   
 
