a07dc45ef767cdf2c6ed8a7dfa8efb9c885d22a4
Muhammed Safvan MP <134296048+MuhammedSafvanMP@users.noreply.github.com>
2026-06-13 12:31:14 +0530

    Update prescription.model.ts

--- hospital-service/src/models/prescription.model.ts
+++ hospital-service/src/models/prescription.model.ts
@@ -16,7 +16,7 @@ TemplateItem.init(
       allowNull: true,
     },
 
-    templateType: {
+   templateType: {
       type: DataTypes.ENUM(
         "demo",
         "custom"
@@ -24,72 +24,10 @@ TemplateItem.init(
       defaultValue: "custom",
     },
 
-    type: {
-      type: DataTypes.ENUM(
-        "text",
-        "table",
-        "patientInfoGrid"
-      ),
+    design: {
+      type: DataTypes.JSON,
       allowNull: false,
-    },
-
-    content: {
-      type: DataTypes.TEXT,
-      defaultValue: "",
-    },
-
-    x: {
-      type: DataTypes.FLOAT,
-      defaultValue: 0,
-    },
-
-    y: {
-      type: DataTypes.FLOAT,
-      defaultValue: 0,
-    },
-
-    width: {
-      type: DataTypes.FLOAT,
-      defaultValue: 100,
-    },
-
-    height: {
-      type: DataTypes.FLOAT,
-      defaultValue: 40,
-    },
-
-    editable: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: true,
-    },
-
-    fontSize: {
-      type: DataTypes.STRING,
-      defaultValue: "text-base",
-    },
-
-    fontWeight: {
-      type: DataTypes.STRING,
-      defaultValue: "",
-    },
-
-    textAlign: {
-      type: DataTypes.ENUM(
-        "left",
-        "center",
-        "right"
-      ),
-      defaultValue: "left",
-    },
-
-    textColor: {
-      type: DataTypes.STRING,
-      defaultValue: "#000000",
-    },
-
-    bgColor: {
-      type: DataTypes.STRING,
-      defaultValue: "transparent",
+      defaultValue: [],
     },
   },
   {
@@ -101,3 +39,12 @@ TemplateItem.init(
 );
 
 export default TemplateItem;
+
+
+
+
+
+
+
+
+
