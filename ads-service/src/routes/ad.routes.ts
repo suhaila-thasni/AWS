b1dbe23ecc03a7cc5aaeb69cb552200dfb1f2f64
Suhaila Thasni <suhailathasni0@gmail.com>
2026-06-13 13:10:24 +0530

    Update ad.routes.ts

--- ads-service/src/routes/ad.routes.ts
+++ ads-service/src/routes/ad.routes.ts
@@ -13,8 +13,8 @@ const router = Router();
 
 router.post("/ads", createAd);
 router.get("/ads",  getAds);
-router.get("/ads/:id",  checkPermission("ad", "view"), getSingleAd);
+router.get("/ads/:id",  getSingleAd);
 router.put("/ads/:id",  updateAd);
-router.delete("/ads/:id", checkPermission("ad", "delete"), deleteAd);
+router.delete("/ads/:id",  deleteAd);
 
 export default router;
