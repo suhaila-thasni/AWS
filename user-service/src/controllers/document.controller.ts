ca49bce11df1fead7d3317f49a7d9a32fc1f2551
Muhammed Safvan MP <134296048+MuhammedSafvanMP@users.noreply.github.com>
2026-06-16 13:29:25 +0530

    Update document.controller.ts

--- user-service/src/controllers/document.controller.ts
+++ user-service/src/controllers/document.controller.ts
@@ -18,17 +18,33 @@ export const createDocument: any = asyncHandler(async (req: Request, res: Respon
   });
 });
 
-export const getDocuments: any = asyncHandler(async (req: Request, res: Response) => {
-  const documents = await Document.findAll({
-    where: { isActive: true },
-    order: [["createdAt", "DESC"]],
-  });
+export const getDocuments = asyncHandler(
+  async (req: Request, res: Response) : Promise<void> => {
+    const normalizeQuery = (value: any) =>
+      Array.isArray(value) ? value[0] : value;
 
-  res.status(200).json({
-    success: true,
-    data: documents,
-  });
-});
+    let { patientId } = req.query;
+
+    patientId = normalizeQuery(patientId);
+
+    const whereClause: any = {};
+
+    if (patientId && !isNaN(Number(patientId))) {
+      whereClause.patientId = Number(patientId);
+    }
+
+    const documents = await Document.findAll({
+      where: whereClause,
+      order: [["createdAt", "DESC"]],
+    });
+
+     res.status(200).json({
+      success: true,
+      data: documents,
+    });
+    return;
+  }
+);
 
 export const getDocument: any = asyncHandler(async (req: Request, res: Response) => {
   const document = await Document.findOne({
