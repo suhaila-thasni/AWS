88a4d0af282c69c68e7b232e50c8e1d71a6af4f2
Muhammed Safvan MP <134296048+MuhammedSafvanMP@users.noreply.github.com>
2026-06-18 13:57:47 +0530

    Update user.service.ts

--- api-gateway/src/services/user.service.ts
+++ api-gateway/src/services/user.service.ts
@@ -23,7 +23,7 @@ breaker.fallback(() => {
 
 export const proxyRequest = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const url = `${SERVICES.USER_SERVICE}${req.originalUrl.replace("/api/users", "/users").replace("/api/patients", "/patients").replace("/api/prescription", "/prescription").replace("/api/vitals", "/vitals").replace("/api/documents", "/documents").replace("/api/lab-results", "/lab-results")}`;
+    const url = `${SERVICES.USER_SERVICE}${req.originalUrl.replace("/api/users", "/users").replace("/api/patients", "/patients").replace("/api/prescription", "/prescription").replace("/api/vitals", "/vitals").replace("/api/documents", "/documents").replace("/api/lab-results", "/lab-results").replace("/api/email-enquiry", "/email-enquiry")}`;
 
     const options = {
       method: req.method,
