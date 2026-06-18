2045be629e87d9146d163f604956c94649c42649
Muhammed Safvan MP <134296048+MuhammedSafvanMP@users.noreply.github.com>
2026-06-18 11:57:37 +0530

    Update 20260414110000-initial-review-rating-schema.js

--- review-service/migrations/20260414110000-initial-review-rating-schema.js
+++ review-service/migrations/20260414110000-initial-review-rating-schema.js
@@ -25,6 +25,10 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
+       rating: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
@@ -38,52 +42,13 @@ module.exports = {
       }
     });
 
-    // Create 'ratings' table
-    await queryInterface.createTable('ratings', {
-      id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      userId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
-      hospitalId: {
-        type: Sequelize.INTEGER,
-        allowNull: true,
-      },
-      doctorId: {
-        type: Sequelize.INTEGER,
-        allowNull: true,
-      },
-      rating: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
+    
 
     // Add indexes for ratings
-    await queryInterface.addIndex('ratings', ['userId', 'doctorId'], {
-      unique: true,
-      name: 'ratings_userId_doctorId_unique'
-    });
-    await queryInterface.addIndex('ratings', ['userId', 'hospitalId'], {
-      unique: true,
-      name: 'ratings_userId_hospitalId_unique'
-    });
+
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('ratings');
     await queryInterface.dropTable('review');
   }
 };
